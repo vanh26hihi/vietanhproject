126b5f51cbec9e1bfd1a80d9efeb9f8d4e95a811
vanh26hihi <vietanhdoan987@gmail.com>
2026-08-24 22:35:20 +0700

    switch entrypoint to final Studio with ClickerTray and advanced keycap

--- src/lovable/main.tsx
+++ src/lovable/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import StudioV2 from "./StudioV2";
+import Studio from "./Studio";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <StudioV2 />
+    <Studio />
   </React.StrictMode>,
 );
