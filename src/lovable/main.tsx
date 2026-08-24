61b4248e354d6a41ba6ea3a22b66020d63b0e9dd
vanh26hihi <vietanhdoan987@gmail.com>
2026-08-24 17:14:36 +0700

    feat: switch lovable base to lazy studio shell

--- src/lovable/main.tsx
+++ src/lovable/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import Studio from "./Studio";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Studio />
   </React.StrictMode>,
 );
